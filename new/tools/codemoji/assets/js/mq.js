de9f54ef93997022987f2a9b1c58fa7456e60092
GITEE <gitee@wujiaxing.cn>
2020-10-14 09:42:16 +0800

    update bsxx

--- new/tools/codemoji/assets/js/mq.js
+++
@@ -1,5 +0,0 @@
-(function (window, Cryptoloji, undefined) {
-
-  Cryptoloji.mq = window.matchMedia("screen and (min-width: 1024px)").matches
-
-})(window, window.Cryptoloji); 
--- new/tools/codemoji/emojiListBuilder/get-not-found-emoji.js
+++
@@ -1,6 +0,0 @@
-
-var notFound = []
-$("span:not(:has(*))").each(function (i, e) { 
-  notFound.push(($(e).text().codePointAt()))
-})
-console.log(JSON.stringify(notFound))
